94a09e8d1309b89816b2b1da5f13822a0b5058ed
Clemens Tolboom <clemens@build2be.com>
2014-10-02 10:46:52 +0200

    Make better config for post.

--- post-rest.php
+++ post-rest.php
@@ -13,8 +13,9 @@ $config = array(
   'password' => "admin",
   'post' => array(
     'node' => array(
-      'field' => 'title',
-      'value' => 'test',
+      'fields' => array(
+        'title' => 'test',
+      ),
       'type' => '/rest/type/node/article'
     )
   )
@@ -24,7 +25,9 @@ function buildPayload($config, $task)
 {
     $payload = array();
     $payload['_links']['type']['href'] = $config['url'] . $config['post'][$task]['type'];
-    $payload[$config['post'][$task]['field']] = array($config['post'][$task]['value']);
+    foreach ($config['post'][$task]['fields'] as $key => $value) {
+      $payload[$key] = array($value);
+    }
     return json_encode($payload);
 }
 
@@ -33,19 +36,21 @@ foreach ($config['post'] as $taskname => $task) {
     $payload = buildPayload($config, $taskname);
     $headers = array();
     $headers['Content-type'] = 'application/json';
-    $response = $client->post('entity/' . $taskname, $headers, $payload)
+    $response = $client->post('' . $taskname, $headers, $payload)
       ->setAuth($config['username'], $config['password'])
       ->send();
 
     echo 'created ' . $taskname . PHP_EOL;
     echo 'Response status: ' . $response->getStatusCode() . PHP_EOL;
     if ($response->getStatusCode() > 299 || $response->getStatusCode() < 200) {
-        echo 'Response body: ' . PHP_EOL;
+        echo 'Response: ' . PHP_EOL;
         try {
+            echo "Success:" . PHP_EOL;
             $body = json_decode($response->getBody(), true);
             $body = print_r($body, true);
             echo $body;
         } catch (Exception $e) {
+            echo "Failure:" . PHP_EOL;
             echo $response->getBody();
         }
     }
