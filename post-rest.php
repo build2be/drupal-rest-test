3512b031b7f10a20746690dc6bf9dd02da10d330
Martijn Braam <martijn@brixit.nl>
2014-10-02 10:57:59 +0200

    Added debug output to post-rest.php

--- post-rest.php
+++ post-rest.php
@@ -8,17 +8,17 @@ $iniConfig = parse_ini_file(__DIR__ . '/rest.ini', false, INI_SCANNER_RAW);
 
 
 $config = array(
-  'url' => $iniConfig['URL'],
-  'username' => "admin",
-  'password' => "admin",
-  'post' => array(
-    'node' => array(
-      'fields' => array(
-        'title' => 'test',
-      ),
-      'type' => '/rest/type/node/article'
+    'url' => $iniConfig['URL'],
+    'username' => "admin",
+    'password' => "admin",
+    'post' => array(
+        'node' => array(
+            'fields' => array(
+                'title' => 'test',
+            ),
+            'type' => '/rest/type/node/article'
+        )
     )
-  )
 );
 
 function buildPayload($config, $task)
@@ -26,19 +26,37 @@ function buildPayload($config, $task)
     $payload = array();
     $payload['_links']['type']['href'] = $config['url'] . $config['post'][$task]['type'];
     foreach ($config['post'][$task]['fields'] as $key => $value) {
-      $payload[$key] = array($value);
+        $payload[$key] = array($value);
     }
     return json_encode($payload);
 }
 
-$client = new Client($config['url']);
+function post($url, $headers, $payload)
+{
+    global $config;
+    $client = new Client($config['url']);
+
+    echo '--------------------' . PHP_EOL;
+    echo 'POST /' . $url . PHP_EOL;
+    foreach ($headers as $key => $value) {
+        echo $key . ': ' . $value . PHP_EOL;
+    }
+    echo PHP_EOL;
+    echo $payload.PHP_EOL;
+    echo '--------------------' . PHP_EOL;
+
+    $response = $client->post($url, $headers, $payload)
+        ->setAuth($config['username'], $config['password'])
+        ->send();
+    return $response;
+}
+
+
 foreach ($config['post'] as $taskname => $task) {
     $payload = buildPayload($config, $taskname);
     $headers = array();
     $headers['Content-type'] = 'application/json';
-    $response = $client->post('' . $taskname, $headers, $payload)
-      ->setAuth($config['username'], $config['password'])
-      ->send();
+    $response = post('' . $taskname, $headers, $payload);
 
     echo 'created ' . $taskname . PHP_EOL;
     echo 'Response status: ' . $response->getStatusCode() . PHP_EOL;
